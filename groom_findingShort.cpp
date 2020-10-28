bfcb9b0d5d99fb4a219b66076ae6cf9b9de9d16b
Youngwoo Park <36143152+parkyw1206@users.noreply.github.com>
2020-10-27 22:45:59 -0700

    Update groom_findingShort.cpp

--- groom_findingShort.cpp
+++ groom_findingShort.cpp
@@ -1,45 +1,88 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
-
-vector<vector<int>> v;
-vector<vector<bool>> visited;
-int N, answer;
-bool dfs(int i , int j){
-	if(i < 0 || j < 0 || i >= N || j >= N)
+int N,dist,dista,distb,distc;
+vector<vector<int>> arr;
+/*
+bool bfs(int x , int y){
+	if(x < 0 || y < 0 || x >= N || y >= N)
 		return false;
-	else if(v[i][j] != 1)
+	else if(arr[x][y] != 1)
 		return false;
-	else if(i == N-1 && j == N-1){
-		answer ++;
-		v[i][j] = 3;
+	else if(x == N-1 && y == N-1){
+		dist++;
 		return true;
 	}
 	else{
-		answer ++;
-		v[i][j] = 3;
-		if(dfs(i-1,j)|| dfs(i,j+1) || dfs(i+1,j) || dfs(i,j-1))
+		arr[x][y] = 3;
+		dist++;
+		if(bfs(x-1,y)  || bfs(x,y+1) || bfs(x+1,y) || bfs(x,y-1))
 			return true;
-		answer--;
-		v[i][j] = 2;
+		dist--;
+		arr[x][y] = 2;
 		return false;
 	}
 }
-int main() {
-	int input;	
-	cin >> N;
-	for(int i = 0 ; i < N; i++){
-		v.push_back(vector<int>());
-		for(int j = 0 ; j < N ; j++){
-			cin >> input;
-			v[i].push_back(input);
+*/
+bool  checkValid(int x , int y){
+	if(x < 0 || y < 0 || x >= N || y >= N)
+		return false;
+	
+	return true;
+}
+void bfs(int startX , int startY){
+	int x = startX , y = startY;
+	dista = 0, distb = 0,distc=0;
+	
+	queue<pair<pair<int,int>,int>> q;
+	bool done = false;
+	q.push({{x,y},0});
+	while(q.size() > 0 && !done){
+		x = q.front().first.first;
+		y = q.front().first.second;
+		distc = q.front().second;
+		arr[x][y] = 3;
+		
+		if(x==N-1 && y == N-1){
+			done = true;
+			distc++;
+			dist = distc;
+			break;			
+		}
+
+		q.pop();
+		
+		if(checkValid(x-1,y) && arr[x-1][y] == 1){
+			q.push({{x-1,y},distc +1});
+		}
+		if(checkValid(x,y+1) && arr[x][y+1] == 1){
+			q.push({{x,y+1},distc +1});			
+		}
+		if(checkValid(x,y-1) && arr[x][y-1] == 1){
+			q.push({{x,y-1},distc +1});
+		}
+		if(checkValid(x+1,y) && arr[x+1][y] == 1){
+			q.push({{x+1,y},distc +1});			
 		}
 	}
-	answer = 0;
-	dfs(0,0);
+}
+int main() {
+	int temp;
+	cin >> N;
 	
-	cout << answer;
+	for(int i = 0 ; i < N; i++){
+		arr.push_back(vector<int>());
+		for(int j = 0 ; j < N; j++){
+			cin >> temp;
+			arr[i].push_back(temp);
+		}
+	}
+
+	dist = 0;
+	bfs(0,0);
+	
+	cout << dist << endl;
 	return 0;
 }
